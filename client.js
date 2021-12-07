b6755407c625213a23ea6b66e38caf2831097637
Christopher Razidlo <cd.razidlo@gmail.com>
2021-12-07 13:33:29 -0600

    pushed bonus to old object

--- client.js
+++ client.js
@@ -40,3 +40,42 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+function NewEmployeeObject( employeeObject ){
+    this.name = name;
+
+} 
+
+function calculateBonus ( array ) {
+  for ( let employee of array ) {
+    let bonusPercentage = 0;
+    if ( employee.reviewRating <= 2 ) {
+       bonusPercentage = 0;
+    }
+    else if ( employee.reviewRating === 3 ) {
+      bonusPercentage = 0.04;
+    } 
+    else if ( employee.reviewRating === 4 ) {
+      bonusPercentage = 0.06;
+    } 
+    else if ( employee.reviewRating === 5 ) {
+      bonusPercentage = 0.1;
+    }
+    if ( employee.employeeNumber.length > 4 ) {
+      bonusPercentage += 0.05;
+    }
+    if ( employee.annualSalary > 65000 ) {
+      bonusPercentage -= 0.01;
+    }
+    if ( bonusPercentage > 0.13 ) {
+      bonusPercentage = 0.13;
+    }
+    if ( bonusPercentage < 0 ) {
+      bonusPercentage = 0;
+    }
+    employee.bonusPercentage = bonusPercentage;
+  }
+}
+
+console.log( calculateBonus ( employees ) );
+
